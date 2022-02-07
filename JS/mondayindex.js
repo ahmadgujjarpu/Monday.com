d441084afa01857b37f7c28e5c096e8bba57e45c
Arslan Ahmad <47588234+Arslanars@users.noreply.github.com>
2022-02-07 16:00:54 +0500

    cors add

--- JS/mondayindex.js
+++ JS/mondayindex.js
@@ -1,18 +1,14 @@
 
-
 function getEndPointData(endPointURL, bindingData,rowId){
     const xhr = new XMLHttpRequest();
     xhr.open("Get", endPointURL);
     xhr.onload = function () {
         const data = JSON.parse(xhr.responseText); 
         bindingData(data,rowId);
-  
     }
     xhr.send();
   }
  
-
-
 function generateCell(title,person,date,status) {
  let template=document.querySelector("template").content.cloneNode(true);
 template.querySelector("#item").innerHTML=title;
@@ -21,7 +17,6 @@ template.querySelector("#date").innerHTML=date;
 template.querySelector("#status").innerHTML=status;
  return template;
 }
-
 
 function bindingData(data,rowId) {
     
@@ -33,7 +28,6 @@ function bindingData(data,rowId) {
         document.getElementById(`${rowId}`).append(generateCell(title,person,date,status));
     }
 }
-
 
 getEndPointData("http://localhost:3005/api/projectplan", bindingData,"project");
 getEndPointData("http://localhost:3005/api/projectTwitter", bindingData,"projectTwitter");
