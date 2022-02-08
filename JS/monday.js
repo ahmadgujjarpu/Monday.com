419067de9350b985d8fd14d59d906a814787004a
ahmadgujjarpu <ahmademmi98@gmail.com>
2022-02-08 11:37:23 +0500

    cors

--- JS/monday.js
+++ JS/monday.js
@@ -1,11 +1,9 @@
 const express =require("express");
 const mysql=require("mysql");
-// const body_parser=require("body-parser");
-// const res = require("express/lib/response");
+const cors=require("cors");
+
 const app=express();
-
-// app.use(body_parser.json());
-
+app.use(cors());
 
  const con=mysql.createConnection(
     {
